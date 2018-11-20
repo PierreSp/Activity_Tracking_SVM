a42b6170946cb0c9b7f4740bd972a19da12ff7e5
Pierre <pierre.springer@tum.de>
2018-11-20 14:01:01 +0100

    Remove class

--- data_processor.py
+++ data_processor.py
@@ -8,47 +8,45 @@ import scipy.io as sio
 ##############################################
 
 
-class Data_processor():
-    """Data Processor for data from the Shimmer 3 device
-       Takes mat files generated from Wake"""
-    def __init__(self, file):
-        self.data = self.load_data(file)
+def load_data(filename):
+    data_2016 = sio.loadmat('data2016.mat')
+    mdata = data_2016["data"]
+    mdtype = mdata.dtype
+    ndata = {n: mdata[n][0, 0] for n in mdtype.names}  # Extract col names
+    return ndata
 
-    def load_data(self, filename):
-        data_2016 = sio.loadmat('data2016.mat')
-        mdata = data_2016["data"]
-        mdtype = mdata.dtype
-        ndata = {n: mdata[n][0, 0] for n in mdtype.names}  # Extract col names
-        return ndata
 
-    def calc_gravitation_components(self, data):
-        """Takes measurements of one dimension and returns the mean """
-        mean_val = np.mean(data)
-        return mean_val
+def calc_gravitation_components(data):
+    """Takes measurements of one dimension and returns the mean """
+    mean_val = np.mean(data)
+    return mean_val
 
-    def _calc_net_acceleration(self, x, y, z):
-        """Takes measurements of all dimension and returns the net acceleration,
-        which is the norm of the measurments (per window)"""
-        net_acceleration = np.sqrt(x**2 + y**2 + z**2)
-        return net_acceleration
 
-    def calc_net_acceleration_dcfree(self, x, y, z):
-        """Takes measurements and returns net acceleration without dc component(mean)"""
-        net_acceleration = self._calc_net_acceleration(x, y, z)
-        net_acceleration_dc = net_acceleration - np.mean(net_acceleration)
-        return net_acceleration_dc
+def _calc_net_acceleration(x, y, z):
+    """Takes measurements of all dimension and returns the net acceleration,
+    which is the norm of the measurments (per window)"""
+    net_acceleration = np.sqrt(x**2 + y**2 + z**2)
+    return net_acceleration
 
-    def create_features(self, data, label):
-        featuredict = {}  # Dict for all features
-        ds_d_type = data[label].dtype
-        ds_d_data = {n: data[label][n][0, 0] for n in ds_d_type.names}  # Extract data with colum names (x,y,z)
-        N = len(ds_d_data["x"])  # Total number of measurements
-        # Add features to dict
-        featuredict["label"] = np.repeat(label, N)
-        featuredict["x_mean"] = self.calc_gravitation_components(ds_d_data["x"])
-        featuredict["y_mean"] = self.calc_gravitation_components(ds_d_data["y"])
-        featuredict["z_mean"] = self.calc_gravitation_components(ds_d_data["z"])
-        print(self.calc_net_acceleration_dcfree(ds_d_data["x"],ds_d_data["y"],ds_d_data["z"]))
+
+def calc_net_acceleration_dcfree(x, y, z):
+    """Takes measurements and returns net acceleration without dc component"""
+    net_acceleration = _calc_net_acceleration(x, y, z)
+    net_acceleration_dc = net_acceleration - np.mean(net_acceleration)
+    return net_acceleration_dc
+
+
+def create_features(data, label):
+    featuredict = {}  # Dict for all features
+    ds_d_type = data[label].dtype
+    ds_d_data = {n: data[label][n][0, 0] for n in ds_d_type.names}  # Extract data with colum names (x,y,z)
+    N = len(ds_d_data["x"])  # Total number of measurements
+    # Add features to dict
+    featuredict["label"] = np.repeat(label, N)
+    featuredict["x_mean"] = calc_gravitation_components(ds_d_data["x"])
+    featuredict["y_mean"] = calc_gravitation_components(ds_d_data["y"])
+    featuredict["z_mean"] = calc_gravitation_components(ds_d_data["z"])
+    print(calc_net_acceleration_dcfree(ds_d_data["x"],ds_d_data["y"],ds_d_data["z"]))
 
 
 # Load data
