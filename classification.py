bae50036607064a0c592bb89d518b545f6939119
Pierre <pierre.springer@tum.de>
2018-11-21 11:07:50 +0100

    Add TSNE dimension reduction

--- classification.py
+++ classification.py
@@ -8,3 +8,9 @@ def cross_validation_svm(data, label, kernel="rbf", gamma="scale", cv=5):
     print(f"Accuracy: {scores.mean()}(+/- {scores.std() * 2})")
 
 def dimension_reduction(data, method="TLSE", ndim=2):
+    if method == "TLSE":
+        embedded_data= TSNE(n_components=ndim).fit_transform(data)
+    else:
+        print(f"Method {method} not available. Use TLSE instead")
+        embedded_data= TSNE(n_components=ndim).fit_transform(data)
+    return embedded_data
